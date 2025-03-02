301f966ac58062c04d4ae0d1bb7c6bbb848920a3
Rajesh Korde <rajesh.korde@gmail.com>
2025-03-02 10:48:51 -0800

    adding scraping and llm call code

--- frontend.py
+++ frontend.py
@@ -31,13 +31,11 @@ def _(mo):
     url_form = (
         mo.md(
             """
-
             {url}
-
             """
         )
         .batch(
-            url=mo.ui.text(label="Recipe URL", full_width=True),
+            url=mo.ui.text(label="Recipe URL", full_width=True, value="https://twokooksinthekitchen.com/best-pad-thai-recipe/"),
         )
         .form(show_clear_button=True, bordered=False)
     )
@@ -46,43 +44,52 @@ def _(mo):
 
 
 @app.cell
-def _(mo):
-    # Create a form with multiple elements
-    form = (
-        mo.md(
-            """
-            **Your form.**
-
-            {name}
-
-            {date}
-            """
-        )
-        .batch(
-            name=mo.ui.text(label="name"),
-            date=mo.ui.date(label="date"),
-        )
-        .form(show_clear_button=True, bordered=True)
-    )
-    form
-    return (form,)
+def _(mo, url_form):
+    status = mo.md(f"Scraping website: {url_form.value["url"]}") if url_form.value else mo.md("")
+    status
+    url_form.value
+    return (status,)
 
 
 @app.cell
-def _(form):
-    form.value
-    return
+def _(mo, scrape_and_convert_to_md, url):
+    content = scrape_and_convert_to_md(url)
+    if not content:
+        status = mo.md("Could not scrape website")
+        mo.stop()
+    return content, status
 
 
 @app.cell
-def _(form, mo, pd):
-    def get_table(form):
-        data = pd.DataFrame([[form.value["name"],form.value["date"]]], columns=["Name", "DOB"])
+def _(UnexpectedModelBehavior, content, extractor_agent, mo):
+    try:
+        result = extractor_agent.run_sync(f"Markdown:\n {content}")
+    except UnexpectedModelBehavior:
+        status = mo.md("Could not extract recipe")
+        mo.stop()
+
+    if not result or not result.data:
+        mo.md("Could not extract recipe")
+        mo.stop()
+    recipe = result.data
+    if not recipe or not recipe.ingredients:
+        mo.md("Could not extract recipe")
+        mo.stop()
+
+    recipe.name
+    recipe.servings
+    return recipe, result, status
+
+
+@app.cell
+def _(mo, pd, recipe, url_form):
+    def get_table(recipe):
+        data = pd.DataFrame([i.model_dump() for i in recipe.ingredients])
         table = mo.ui.table(data)
         return table
     
 
-    mo.md('') if not form.value or not form.value["name"] else get_table(form)
+    mo.md('') if not url_form.value or not url_form.value["url"] or not recipe else get_table(recipe)
 
     return (get_table,)
 
